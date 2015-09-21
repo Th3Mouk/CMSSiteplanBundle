e28fdb94b6ca9a0d979f459d6cda8969edc8f15b
Jérémy Marodon <marodon.jeremy@gmail.com>
2015-09-21 10:00:29 +0200

    [ADD] Basic menu

--- Menu/BasePlanBuilder.php
+++ Menu/BasePlanBuilder.php
@@ -1,7 +1,36 @@
 <?php
 
+namespace Th3Mouk\CMSSiteplanBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Doctrine\ORM\EntityManager;
 
 class BasePlanBuilder
 {
+    protected $factory;
 
+    protected $em;
+
+    /**
+     * @param FactoryInterface $factory
+     */
+    public function __construct(FactoryInterface $factory, EntityManager $em)
+    {
+        $this->factory = $factory;
+        $this->em = $em;
+    }
+
+    public function createPlanMenu()
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->addChild('Homepage', array('route' => 'app_home'));
+
+        return $menu;
+    }
+
+    protected function getPagesRepository()
+    {
+        return $this->em->getRepository('Id4vMenuBundle:MenuItem');
+    }
 }
